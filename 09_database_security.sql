e6dc787ad2e52b3db9d1106eee46fcd98f47b91c
irfan-24115940 <irfan080206@students.amikom.ac.id>
2026-07-26 01:53:02 +0700

    SultanSyekhuAlyWafa-commit

--- 09_database_security.sql
+++ 09_database_security.sql
@@ -8,22 +8,70 @@ CREATE ROLE IF NOT EXISTS role_admin;
 CREATE ROLE IF NOT EXISTS role_resepsionis;
 CREATE ROLE IF NOT EXISTS role_analis;
 
--- Privilege pada Tabel
-GRANT ALL PRIVILEGES ON sistem_reservasi_hotel.* TO role_admin;
-GRANT SELECT, INSERT, UPDATE ON sistem_reservasi_hotel.reservasi TO role_resepsionis;
+-- =====================================================
+-- Privilege Tabel
+-- =====================================================
 
--- Privilege pada View
-GRANT SELECT ON sistem_reservasi_hotel.v_reservasi_selesai TO role_analis;
+GRANT ALL PRIVILEGES
+ON sistem_reservasi_hotel.*
+TO role_admin;
 
--- Privilege pada Stored Procedure
-GRANT EXECUTE ON PROCEDURE sistem_reservasi_hotel.sp_audit_kamar_pemeliharaan TO role_admin;
-GRANT EXECUTE ON PROCEDURE sistem_reservasi_hotel.sp_hitung_total_reservasi_tamu TO role_resepsionis;
+GRANT SELECT, INSERT, UPDATE
+ON sistem_reservasi_hotel.reservasi
+TO role_resepsionis;
 
--- Privilege pada Stored Function
-GRANT EXECUTE ON FUNCTION sistem_reservasi_hotel.fn_total_kamar_tersedia TO role_resepsionis;
-GRANT EXECUTE ON FUNCTION sistem_reservasi_hotel.fn_hitung_biaya_menginap TO role_analis;
+GRANT SELECT
+ON sistem_reservasi_hotel.kamar
+TO role_analis;
 
+-- =====================================================
+-- Privilege View
+-- =====================================================
+
+GRANT SELECT
+ON sistem_reservasi_hotel.v_reservasi_selesai
+TO role_admin;
+
+GRANT SELECT
+ON sistem_reservasi_hotel.v_kontak_tamu
+TO role_resepsionis;
+
+GRANT SELECT
+ON sistem_reservasi_hotel.v_reservasi_selesai
+TO role_analis;
+
+-- =====================================================
+-- Privilege Procedure
+-- =====================================================
+
+GRANT EXECUTE
+ON PROCEDURE sistem_reservasi_hotel.sp_audit_kamar_pemeliharaan
+TO role_admin;
+
+GRANT EXECUTE
+ON PROCEDURE sistem_reservasi_hotel.sp_hitung_total_reservasi_tamu
+TO role_resepsionis;
+
+GRANT EXECUTE
+ON PROCEDURE sistem_reservasi_hotel.sp_audit_kamar_pemeliharaan
+TO role_analis;
+
+-- =====================================================
+-- Privilege Function
+-- =====================================================
+
+GRANT EXECUTE
+ON FUNCTION sistem_reservasi_hotel.fn_total_kamar_tersedia
+TO role_resepsionis;
+
+GRANT EXECUTE
+ON FUNCTION sistem_reservasi_hotel.fn_hitung_biaya_menginap
+TO role_analis;
+
+-- =====================================================
 -- Inisialisasi Role ke User
+-- =====================================================
+
 GRANT role_admin TO 'user_admin'@'localhost';
 GRANT role_resepsionis TO 'user_resepsionis'@'localhost';
 GRANT role_analis TO 'user_analis'@'localhost';
