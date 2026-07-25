a2061416f7e6d7bf6826bf918a84e9dffc77c2f6
irfan-24115940 <irfan080206@students.amikom.ac.id>
2026-07-26 01:52:29 +0700

    M.DhaniFavian-commit

--- 07_index.sql
+++ 07_index.sql
@@ -1,6 +1,16 @@
--- Index via CREATE INDEX (Composite Index)
-CREATE INDEX idx_tipe_status ON kamar (id_tipe, status_kamar);
+-- Metode 1: Composite Index via CREATE TABLE
+CREATE TABLE log_index_demo (
+    id_user INT,
+    waktu_login DATETIME,
+    aktivitas VARCHAR(100),
+    PRIMARY KEY (id_user, waktu_login)
+);
 
--- Index via ALTER TABLE Constraint
-ALTER TABLE reservasi 
-ADD CONSTRAINT unique_tamu_checkin UNIQUE (id_tamu, tgl_checkin);
+-- Metode 2: Composite Index via CREATE INDEX
+CREATE INDEX idx_tipe_status
+ON kamar (id_tipe, status_kamar);
+
+-- Metode 3: Composite Index via ALTER TABLE
+ALTER TABLE reservasi
+ADD CONSTRAINT unique_tamu_checkin
+UNIQUE (id_tamu, tgl_checkin);
