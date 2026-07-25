c4a1e510555b29d5d25eb1a588b066432c1ee1d8
irfan-24115940 <irfan080206@students.amikom.ac.id>
2026-07-25 18:20:22 +0700

    M.DhaniFavian-commit

--- 07_index.sql
+++ 07_index.sql
@@ -1,9 +1,6 @@
--- =============================================
--- 07. INDEXES
--- =============================================
+-- Index via CREATE INDEX (Composite Index)
+CREATE INDEX idx_tipe_status ON kamar (id_tipe, status_kamar);
 
--- Composite Index via CREATE INDEX
-CREATE INDEX idx_tipe_status ON kamar(id_tipe, status_kamar);
-
--- Composite Index via ALTER TABLE Constraint
-ALTER TABLE reservasi ADD CONSTRAINT unique_tamu_checkin UNIQUE (id_tamu, tgl_checkin);
+-- Index via ALTER TABLE Constraint
+ALTER TABLE reservasi 
+ADD CONSTRAINT unique_tamu_checkin UNIQUE (id_tamu, tgl_checkin);
